d63a3d3595c9e8c3dc5f4f3b5c29a9f4297a187d
Miles Lucas <mdlucas@iastate.edu>
2018-02-05 20:39:57 -0600

    semi-working model

--- hw2/prob3.1.py
+++ hw2/prob3.1.py
@@ -14,28 +14,38 @@ rho = 1e-4 # g / cm^3
 T = np.linspace(1e4, 2e5, 1000)
 
 
+
+
+
 def eqns(p, T):
-    z0, z1, z2 = p
+    z1, z2 = p
+    if (z1 < 0 or z1 > 1) or (z2 < 0 or z2 > 1):
+        return [np.inf, np.inf]
     A = rho * NA * (z1 + 2 * z2) / 4
     C = (2 * pi * me * k * T) ** (3/2) / h**3
 
-    eq1 = A * z1 / z0 - 4 * C * np.exp(-1 * chi1 / (k * T))
+    eq1 = A * z1 / (1 - z1 - z2) - 4 * C * np.exp(-1 * chi1 / (k * T))
     eq2 = A * z2 / z1 - C * np.exp(-1 * chi2 / (k * T))
-    eq3 = z0 + z1 + z2 - 1
-    return (eq1, eq2, eq3)
+    return (eq1, eq2)
 
 
 if __name__ == '__main__':
-    sols = np.asarray([fsolve(eqns, x0=(0.3, 0.3, 0.3), args=(t)) for t in T])
-    z0, z1, z2 = sols.T
+    x0 = [
+        np.concatenate([np.linspace(0, 1, 500), np.linspace(1, 0, 500)]),
+        np.linspace(0, 1, 1000)
+    ]
+
+    sols = np.asarray([fsolve(eqns, x0=x, args=(t)) for t, x in zip(T, np.transpose(x0))])
+    z1, z2 = sols.T
+    z0 = 1 - z1 - z2
     ze = z1 + 2 * z2
     plt.style.use('seaborn')
     plt.title(rf'z1 and z2 for $\rho = {rho}$')
 
-    plt.plot(T, z0, label='z0')
-    plt.plot(T, z1, label='z1')
-    plt.plot(T, z2, label='z2')
-    plt.plot(T, ze, c='r', label='ze')
+    plt.semilogx(T, z0, label='z0')
+    plt.semilogx(T, z1, label='z1')
+    plt.semilogx(T, z2, label='z2')
+    # plt.plot(T, ze, c='r', label='ze')
 
     plt.xlabel('T (K)')
     plt.ylim(0, 1)
