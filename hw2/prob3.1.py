527153c0c844a98be93ba5aad1a583f03420a737
Miles Lucas <mdlucas@iastate.edu>
2018-03-20 10:29:59 -0500

    added hw4

--- hw2/prob3.1.py
+++ hw2/prob3.1.py
@@ -4,24 +4,21 @@ from scipy.optimize import least_squares
 
 # Definitions
 pi = np.pi
-me = 9.1095e-28 # g
-h = 6.6262e-27 # erg s
-k = 1.3803e-16 # erg / K
-chi1 = 3.939e-11 # erg
-chi2 = 8.718e-11 # erg
+me = 9.1095e-28  # g
+h = 6.6262e-27  # erg s
+k = 1.3803e-16  # erg / K
+chi1 = 3.939e-11  # erg
+chi2 = 8.718e-11  # erg
 NA = 6.02e23
-rho = 1e-4 # g / cm^3
+rho = 1e-4  # g / cm^3
 T = np.linspace(1e4, 2e5, 1000)
-
-
-
 
 
 def eqns(p, T):
     z1, z2 = p
 
     A = rho * NA * (z1 + 2 * z2) / 4
-    C = (2 * pi * me * k * T) ** (3/2) / h**3
+    C = (2 * pi * me * k * T) ** (3 / 2) / h**3
 
     eq1 = A * z1 / (1 - z1 - z2) - 4 * C * np.exp(-1 * chi1 / (k * T))
     eq2 = A * z2 / z1 - C * np.exp(-1 * chi2 / (k * T))
@@ -35,7 +32,8 @@ if __name__ == '__main__':
         z1 = []
         z2 = []
         for t in T:
-            sol = least_squares(eqns, x0=(0.3, 0.3), args=(t, ), bounds=([0, 0], [1, 1]))
+            sol = least_squares(eqns, x0=(0.3, 0.3), args=(
+                t, ), bounds=([0, 0], [1, 1]))
             z1.append(sol.x[0])
             z2.append(sol.x[1])
         z1, z2 = np.array([z1, z2])
@@ -47,7 +45,6 @@ if __name__ == '__main__':
     idx0 = np.argwhere(np.diff(np.sign(z0 - z1)) != 0)
     idx1 = np.argwhere(np.diff(np.sign(z1 - z2)) != 0)
 
-
     plt.style.use('seaborn')
     plt.title(rf'$z_i$ for $\rho = {rho:.2e}$')
 
@@ -56,14 +53,14 @@ if __name__ == '__main__':
     plt.semilogx(T, z2, label=r'$z_2$')
 
     plt.plot(T[idx0], z1[idx0], 'ko')
-    plt.text(T[idx0]+2000, z1[idx0], f'{int(T[idx0][0][0]):.2e}')
+    plt.text(T[idx0] + 2000, z1[idx0], f'{int(T[idx0][0][0]):.2e}')
     plt.plot(T[idx1], z1[idx1], 'ko')
-    plt.text(T[idx1]+5000, z1[idx1], f'{int(T[idx1][0][0]):.2e}')
+    plt.text(T[idx1] + 5000, z1[idx1], f'{int(T[idx1][0][0]):.2e}')
     # plt.semilogx(T, ze, c='r', label=r'z_e')
 
     plt.xlabel('T (K)')
     plt.ylabel('z')
     plt.legend()
 
-    plt.savefig('prob3.1.png')
+    # plt.savefig('prob3.1.png')
     plt.show()
